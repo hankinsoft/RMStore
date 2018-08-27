352db2857c695abe0d54e93449dedb5ac551fd95
Kyle Hankinson <kyle@hankinsoft.com>
2018-08-27 13:55:47 -0300

    Update to framework/wrapper name.

--- RMStoreFramework/RMStoreFramework.h
+++ RMStoreFramework/RMStoreFramework.h
@@ -15,5 +15,4 @@ FOUNDATION_EXPORT double RMStoreFrameworkVersionNumber;
 FOUNDATION_EXPORT const unsigned char RMStoreFrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <RMStoreFramework/PublicHeader.h>
-
-
+#import "RMStore.h"
