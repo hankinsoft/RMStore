9a9880294dc326f41c0a976a5a93617b993a6ce4
Kyle Hankinson <kyle@hankinsoft.com>
2018-08-27 13:40:09 -0300

    Framework for macOS/iOS

---
+++ RMStoreFramework/RMStoreFramework.h
@@ -0,0 +1,19 @@
+//
+//  RMStoreFramework.h
+//  RMStoreFramework
+//
+//  Created by kylehankinson on 2018-08-27.
+//  Copyright © 2018 Robot Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RMStoreFramework.
+FOUNDATION_EXPORT double RMStoreFrameworkVersionNumber;
+
+//! Project version string for RMStoreFramework.
+FOUNDATION_EXPORT const unsigned char RMStoreFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RMStoreFramework/PublicHeader.h>
+
+
