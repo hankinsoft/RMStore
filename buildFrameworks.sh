a3b2838e6d7b07d015f2f30ef6d95168e3964c1e
Kyle Hankinson <kyle.hankinson@gmail.com>
2022-02-16 13:36:02 -0400

    + Renamed.

--- buildFrameworks.sh
+++ buildFrameworks.sh
@@ -2,6 +2,6 @@ xcodebuild archive -scheme "RMStoreFramework-macOS" -archivePath "./build/macos.
 xcodebuild archive -scheme "RMStoreFramework-iOS" -archivePath "./build/ios.xarchive" -sdk iphoneos SKIP_INSTALL=NO
 xcodebuild archive -scheme "RMStoreFramework-iOS" -archivePath "./build/ios_sim.xarchive" -sdk iphonesimulator SKIP_INSTALL=NO
 
-mv build/macos.xarchive.xcarchive/Products/Library/Frameworks/RMStore.framework "Frameworks/RMStore.xcframework/macos-arm64_x86_64"
-mv build/ios.xarchive.xcarchive/Products/Library/Frameworks/RMStore.framework "Frameworks/RMStore.xcframework/ios-arm64_armv7"
-mv build/ios_sim.xarchive.xcarchive/Products/Library/Frameworks/RMStore.framework "Frameworks/RMStore.xcframework/ios-arm64_i386_x86_64-simulator"
+mv build/macos.xarchive.xcarchive/Products/Library/Frameworks/RMStoreFramework.framework "Frameworks/RMStoreFramework.xcframework/macos-arm64_x86_64"
+mv build/ios.xarchive.xcarchive/Products/Library/Frameworks/RMStoreFramework.framework "Frameworks/RMStoreFramework.xcframework/ios-arm64_armv7"
+mv build/ios_sim.xarchive.xcarchive/Products/Library/Frameworks/RMStoreFramework.framework "Frameworks/RMStoreFramework.xcframework/ios-arm64_i386_x86_64-simulator"
